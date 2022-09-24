6fb498e6ce50c91fb13cd162c723c97918c0c9e3
William Paradise <williamparadise@Williams-MacBook-Air.local>
2022-09-24 13:14:36 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+year = input("What year were you born in?")
+age = (2022 - int(year))
+nextage = (2022 - int(year) + 1)
+print(f"You were born in {year}? That means you are {age}, and you will be {nextage} next year.")
